b163d0512fa614d129e2acbd6688c05ad62bd168
Ratsela <ratselamarch@gmail.com>
2014-08-21 11:32:09 +0200

    Blinds App

--- js/areaslider.js
+++ js/areaslider.js
@@ -1,49 +1,28 @@
 sliderInt=1;
-sliderNext=2;
 count = $("#main_area >img").size();
 $(document).ready(function(){
-$("#main_area > img#1").fadeIn(600);
-	//startSlider();
+	$("#main_area > img#1").fadeIn(600);
 });
-function startSlider(){
-	count = $("#main_area >img").size();
-	loop = setInterval(function(){
-					
-	if(sliderNext > count)
-	{
-		sliderNext=1;
-		sliderInt=1;
-	}
-					
-	$("#main_area >img").fadeOut(600);
-	$("#main_area >img#" + sliderNext).fadeIn(600);
-	sliderInt = sliderNext;
-	sliderNext = sliderNext + 1;
-	},3000)
-	}
-function stopLoop(){
-	window.clearInterval(loop);
-}
-function prev(){
-	newSlide = sliderInt - 1;
-	showSlide(newSlide);
-	}
 function next(){
-	newSlide = sliderInt + 1;
-	showSlide(newSlide);
-	}
-function showSlide(id){
-	startSlider();
-	stopLoop();
-	if(id > count){
-		id = 1;
-	}
-	else if(id < 1){
-		id = count;
+		count = $("#main_area >img").size();
+		sliderInt += 1;
+		if(sliderInt <= count){
+			$("#main_area >img").fadeOut(600);
+			$("#main_area >img#" + sliderInt).fadeIn(600);
 		}
-	$("#main_area >img").fadeOut(600);
-	$("#main_area >img#" + sliderNext).fadeIn(600);
-	sliderInt = id;
-	sliderNext = id + 1;
+		else
+		{
+			alert("No more images");
+		}
 	}
-	
+function prev(){
+		sliderInt  = sliderInt - 1;
+		if(sliderInt > 0)
+		{
+			$("#main_area >img").fadeOut(600);
+			$("#main_area >img#" + sliderInt).fadeIn(600);
+		}
+		else{
+			alert("No more images");
+		}
+	}
