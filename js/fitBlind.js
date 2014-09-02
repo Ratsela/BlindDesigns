f6ecbd202aa43df45777057cabd2bb5b5c4ab4f5
Ratsela <ratselamarch@gmail.com>
2014-09-02 11:36:19 +0200

    Blinds App

---
+++ js/fitBlind.js
@@ -0,0 +1,92 @@
+		function message(id)
+			{
+				var roomID = getRoomID();
+				//alert('Room '+roomID);
+				//$("#main_area >img").fadeOut(600);
+				//$("#main_area >img#44").fadeIn(600);
+				
+				
+				
+				if(roomID == 3)
+				{
+					if(id == 4)
+					{
+						$("#main_area >img").fadeOut(5);
+						$("#main_area >img#44").fadeIn(5);
+					}
+					else if(id == 2)
+					{
+						$("#main_area >img").fadeOut(5);
+						$("#main_area >img#55").fadeIn(5);
+					}
+				}
+				
+				/*switch(id){
+					case 1:
+						if(roomID == 1){
+							alert('Room '+roomID);
+						}
+						else if(roomID == 2){
+							alert('Room '+roomID);
+						}
+						else if(roomID == 3){
+							alert('Room '+roomID);
+						}
+						else if(roomID == 4){
+							//alert('Room '+roomID);
+							$("#main_area >img").fadeOut(600);
+							$("#main_area >img#44").fadeIn(600);
+						}
+						break;
+					/*case 2:
+						if(roomID == 1){
+							alert('Room '+roomID);
+						}
+						else if(roomID == 2){
+							alert('Room '+roomID);
+						}
+						else if(roomID == 3){
+							alert('Room '+roomID);
+						}
+						else if(roomID == 4){
+							//alert('Room '+roomID);
+								$("#main_area >img").fadeOut(600);
+							$("#main_area >img#44").fadeIn(600);
+						}
+						break;
+					case 3:
+						if(roomID == 1){
+							alert('Room '+roomID);
+						}
+						else if(roomID == 2){
+							alert('Room '+roomID);
+						}
+						else if(roomID == 3){
+							alert('Room '+roomID);
+						}
+						else if(roomID == 4){
+							//alert('Room '+roomID);  
+								$("#main_area >img").fadeOut(600);
+							$("#main_area >img#44").fadeIn(600);
+						}
+						break;
+					case 4:
+						if(roomID == 1){
+							alert('Room '+roomID);
+						}
+						else if(roomID == 2){
+							alert('Room '+roomID);
+						}
+						else if(roomID == 3){
+							alert('Room '+roomID);
+						}
+						else if(roomID == 4){
+							//alert('Room '+roomID);
+							$("#main_area >img").fadeOut(600);
+							$("#main_area >img#44").fadeIn(600);
+						}
+						break;
+					default:
+						alert('Error');
+				}*/
+			}
